87a87d1d054dcae5bf72b3f0050ddff071b32bcb
Sofus Hjort Helmer <102423577+sofush@users.noreply.github.com>
2024-10-28 11:31:19 +0100

    fix: make hero properties required

--- src/heroValidator.js
+++ src/heroValidator.js
@@ -3,20 +3,24 @@ import Joi from 'joi';
 const heroValidator = Joi.object({
 	id: Joi
 		.number()
+		.required()
 		.integer()
 		.min(1),
 
 	name: Joi
 		.string()
+		.required()
 		.min(1),
 
 	alias: Joi
 		.string()
+		.required()
 		.optional()
 		.min(1),
 
 	powers: Joi
 		.array()
+		.required()
 		.items(Joi.string().min(1))
 });
 
